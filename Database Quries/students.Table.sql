3a9a1f6572b34b975efa48982966071da017372f
HASEEB ALI <75139477+Haseb-ali@users.noreply.github.com>
2020-12-01 16:09:05 +0500

    Add files via upload

---
+++ Database Quries/students.Table.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+    [fullname] TEXT NULL, 
+    [education] TEXT NULL, 
+    [address] TEXT NULL, 
+    [contactno] TEXT NULL, 
+    [age] TEXT NULL, 
+    [email] TEXT NULL, 
+    [status] TEXT NULL
+)
